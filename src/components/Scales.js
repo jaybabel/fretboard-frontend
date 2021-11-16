a9dd8b1c0daa56d21b4f5448865a8e0cbeac192e
jaybabel <BabelJayE@JohnDeere.com>
2021-11-16 17:50:56 -0600

    Additional testing on single scale

--- src/components/Scales.js
+++ src/components/Scales.js
@@ -6,7 +6,8 @@ const scaleId = 1
 function Scales(props) {
     console.log('scales.js ', props)
     const scalesList = props.scales.map((scale) => scale);
-    console.log('scale list: ', scalesList) 
+    // let scalesCount = scalesList.length;
+    // console.log('scales length: ', scalesList.length) 
     // render(); {
     return (
         <div className="scaleSection">
@@ -14,17 +15,15 @@ function Scales(props) {
                 <form className="pageScales">
                     <input type="submit" className="btnScale" value="<"></input>
                     <option selected value=""></option>
-                    {/* {props.scales.map((scalekey, index) => { */}
-                        {/* return ( */}
-                            <li key={scaleId}>    
-                                {/* {props.scales[index].scalename}
-                                <img src={props.scales[index].imageurl} alt="scale" ></img> */}
+                    {props.scales.map((scalekey, index) => { 
+                        return (
+                            <li key={scaleId}> 
                                 <h4>{scalesList[scaleId].scalename}</h4>
                                 <img src={scalesList[scaleId].imageurl} alt="scaleList"></img>
                             </li>
-                        {/* ) */}
-                      {/* })
-                    }  */}
+                         ) 
+                       })
+                    }  
                     <input type="submit" className="btnScale"  value=">"></input>
                 </form>
             </div>
