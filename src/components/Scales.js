7d7b58e6dd22fb79ecac4301e7a229bc5d140c95
jaybabel <BabelJayE@JohnDeere.com>
2021-11-16 16:20:34 -0600

    Displays a single scale at a time. Still needs button functions

--- src/components/Scales.js
+++ src/components/Scales.js
@@ -14,15 +14,17 @@ function Scales(props) {
                 <form className="pageScales">
                     <input type="submit" className="btnScale" value="<"></input>
                     <option selected value=""></option>
-                    {props.scales.map((scalekey, index) => {
-                        return (
-                            <li key={index}>    
-                                {props.scales[scaleId].scalename}
-                                <img src={props.scales[scaleId].imageurl} alt="scale" ></img>
+                    {/* {props.scales.map((scalekey, index) => { */}
+                        {/* return ( */}
+                            <li key={scaleId}>    
+                                {/* {props.scales[index].scalename}
+                                <img src={props.scales[index].imageurl} alt="scale" ></img> */}
+                                <h4>{scalesList[scaleId].scalename}</h4>
+                                <img src={scalesList[scaleId].imageurl} alt="scaleList"></img>
                             </li>
-                        )
-                      })
-                    } 
+                        {/* ) */}
+                      {/* })
+                    }  */}
                     <input type="submit" className="btnScale"  value=">"></input>
                 </form>
             </div>
